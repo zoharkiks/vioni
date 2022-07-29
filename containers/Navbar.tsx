ffd368189b53bc77299f15cff970f8c1a507bf5d
zoharkiks <zoharkiks@gmail.com>
2022-07-29 15:10:02 +0530

    Add menu icon for mobile screens

--- containers/Navbar.tsx
+++ containers/Navbar.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import {images} from '../constants'
+import { Icon } from '@iconify/react';
+
 
 const Navbar = () => {
   return (
-    <div className='flex'>
-      <div className="flex">
+    <div className='flex justify-between items-center '>
+      <div className="flex justify-center items-center font-bold text-2xl space-x-4">
         <img src={images.logo.src} alt="" />
+        <div className="h1">Vioni</div>
       </div>
+      <Icon className='h-8 w-8' icon="heroicons-solid:menu-alt-3" />
     </div>
   )
 }
