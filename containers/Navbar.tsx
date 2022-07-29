67c28b6aef725e4db9202b49eb185ab2f5938193
zoharkiks <zoharkiks@gmail.com>
2022-07-29 20:17:10 +0530

    Add toggle menu functionality on mob. devices

--- containers/Navbar.tsx
+++ containers/Navbar.tsx
@@ -1,18 +1,51 @@
-import React from 'react'
-import {images} from '../constants'
-import { Icon } from '@iconify/react';
-
+import React, { useState } from "react";
+import { images } from "../constants";
+import { Icon } from "@iconify/react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className='flex justify-between items-center '>
-      <div className="flex justify-center items-center font-bold text-2xl space-x-4">
+    <nav className="flex items-center justify-between ">
+      <div className="flex items-center justify-center space-x-4 text-2xl font-bold">
         <img src={images.logo.src} alt="" />
         <div className="h1">Vioni</div>
       </div>
-      <Icon className='h-8 w-8' icon="heroicons-solid:menu-alt-3" />
-    </div>
-  )
-}
+      <Icon
+        onClick={handleMenu}
+        className="h-8 w-8 cursor-pointer"
+        icon="heroicons-solid:menu-alt-3"
+      />
+      {/* Toggle Menu on Mob devices */}
+      {open && (<div className="fixed right-4 top-4 flex w-full max-w-sm rounded-lg bg-primary p-6 shadow-lg">
+        <button className="absolute top-4 right-4 flex items-center justify-center ">
+          <Icon
+            onClick={handleMenu}
+            className="h-6 w-6 text-white"
+            icon="eva:close-fill"
+          />
+        </button>
+        <ul className="space-y-6 font-bold text-white">
+          <li>
+            <a href="">Services</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Help</a>
+          </li>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+        </ul>
+      </div> ) }
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
